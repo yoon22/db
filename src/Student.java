13dde2615a65d73275bbc78d41f85a503027ca82
yoon22 <11yoon30@gmail.com>
2019-03-27 09:41:45 +0900

    33

---
+++ src/Student.java
@@ -0,0 +1,22 @@
+
+public class Student {
+	private String name;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "해당 학생의 이름은 " + name + "이며 나이는 " + age + "입니다.";
+	}
+	
+}
